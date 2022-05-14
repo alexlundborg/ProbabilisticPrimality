099c523acceaa2125da97fd474633d52f077cfca
Alexander Lundborg <alexander.lundborg@gmail.com>
2022-05-14 21:30:52 +0200

    Descriptions added for api and endpoints

--- ProbabilisticPrimality/Program.cs
+++ ProbabilisticPrimality/Program.cs
@@ -6,8 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddFastEndpoints();
-builder.Services.AddSwaggerDoc();
-
+builder.Services.AddSwaggerDoc(settings =>
+{
+    settings.Title = "Probabilistic Primality Test";
+    settings.Version = "v1";
+    settings.Description = "Primality test using witness numbers. Witness numbers are numbers that declare whether an odd" +
+                           "integer greater than 2 is prime or composite.";
+});
 var app = builder.Build();
 
 app.UseFastEndpoints();
