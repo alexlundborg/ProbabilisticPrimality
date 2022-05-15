d3a0abada3e3866fcdb0bb8e2974347ee5586b9a
Alexander Lundborg <alexander.lundborg@gmail.com>
2022-05-15 20:06:19 +0200

    Minor fixes: - space added in Program.cs - comma added in descriptions

--- ProbabilisticPrimality/Summaries/TestWithKWitnessesSummary.cs
+++ ProbabilisticPrimality/Summaries/TestWithKWitnessesSummary.cs
@@ -9,7 +9,7 @@ public class TestWithKWitnessesSummary : Summary<TestWithKWitnessesEndpoint>
     public TestWithKWitnessesSummary()
     {
         Summary = "Test primality of an integer using a specified number of randomly selected witness numbers less than the integer to test";
-        Description = "If the test result says that the integer to test is composite, you can be sure that it is composite. However certain witness numbers are liars and will incorrectly declare that an integer is prime. At most 1/4 of witnesses are liars. if the integer to test is composite then running k iterations of this primality test will declare the integer to be probably prime with a probability at most 4 to the power of −k. Read more here: https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Accuracy";
+        Description = "If the test result says that the integer to test is composite, you can be sure that it is composite. However, certain witness numbers are liars and will incorrectly declare that an integer is prime. At most 1/4 of witnesses are liars. if the integer to test is composite then running k iterations of this primality test will declare the integer to be probably prime with a probability at most 4 to the power of −k. Read more here: https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Accuracy";
         Response<TestWithKWitnessesResponse>(200, "Test was successful");
         Response<ValidationFailureResponse>(400, "The request did not pass validation");
     }
